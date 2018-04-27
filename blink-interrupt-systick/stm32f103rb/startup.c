e50e8f93a085eac1c1ca75c44dc2473631df5244
DenkiStuff <10386249+DenkiStuff@users.noreply.github.com>
2018-04-27 12:15:54 +0900

    Added *.map 	modified:   .gitignore
    corrected .bss section initialization in startup.c and linker script.
    	modified:   blink-interrupt-systick/stm32f103rb/Makefile
    	deleted:    blink-interrupt-systick/stm32f103rb/interrupt.c
    	modified:   blink-interrupt-systick/stm32f103rb/main.ld
    	modified:   blink-interrupt-systick/stm32f103rb/startup.c
    
    corrected the placement of the stack point initializer
    	modified:   simplest/stm32f103rb/Makefile
    	modified:   simplest/stm32f103rb/main.ld

--- blink-interrupt-systick/stm32f103rb/startup.c
+++ blink-interrupt-systick/stm32f103rb/startup.c
@@ -13,7 +13,7 @@ void __attribute__ ((section(".after_vectors"),noreturn)) Reset_Handler (void)
 	// Initialize bss section by iterating and clearing word by word.
 	// It is assumed that the pointers are word aligned.
 	unsigned int *p = &__bss_start__;
-	while (p <= &__bss_end__) {
+	while (p < &__bss_end__) {
 		*p++ = 0;	
 	}
 
